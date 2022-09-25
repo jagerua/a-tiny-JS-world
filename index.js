62c539ec0e835d7cf0a3a29bfa9b6d1de35ec1bf
jagerua <66270398+jagerua@users.noreply.github.com>
2022-09-25 19:06:22 +0300

    a-tiny-js-world

--- index.js
+++ index.js
@@ -2,12 +2,84 @@ import { print } from './js/lib.js';
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/jagerua/a-tiny-JS-world.git
+   Web app: https://jagerua.github.io/a-tiny-JS-world/
    */
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+
+class Animals {
+   constructor (name, friends, gender, species, saying, legs) {
+      this.name = name;
+      this.friends = friends;
+      this.gender = gender;
+      this.species = species;
+      this.saying = saying;
+      this.legs = legs;
+   }
+   getDescription() {
+      return `This ${this.saying} name <strong>${this.name}</strong>. ${this.species}. This is ${this.gender}. And ${this.name}\`s friends are ${this.friends.join(', ')} around. Legs ${this.legs}.`;
+   }
+};
+
+class Dog extends Animals {
+   constructor (name, friends, gender) {
+      super(name, friends, gender, 'Woof-woof!', 'dog', 4);
+   }
+};
+
+class Cat extends Animals {
+   constructor (name, friends, gender) {
+      super(name, friends, gender, 'May!', 'cat', 4);
+   }
+};
+
+class People {
+   constructor (name, friends, gender, species, saying, legs, hands) {
+      this.name = name;
+      this.friends = friends;
+      this.gender = gender;
+      this.species = species;
+      this.saying = saying;
+      this.legs = legs;
+      this.hands = hands;
+   }
+   getDescriptionPeople() {
+      return `This ${this.saying} name <strong>${this.name}</strong>. ${this.saying}! This is ${this.gender}. And ${this.name}\`s friends are ${this.friends.join(', ')}. Has ${this.legs} legs, ${this.hands} hands.`;
+   }
+};
+
+
+   class Men extends People {
+      constructor (name, friends, gender, saying) {
+         super(name, friends, gender, saying, 'human', 2, 2);
+         
+      }
+   }
+
+   class SuperWoman extends People {
+      constructor (name, friends, gender, saying) {
+         super(name, friends, gender, saying, 'human', 2, 2);
+         this.power = 'SuperWoman.';
+      }
+       getDescriptionPeople() {
+         return super.getDescriptionPeople() + ` I am ${this.power}`
+       }
+        
+   };
+
+
+const dog = new Dog ('Jack', ['All'], 'male');
+const cat = new Cat ('Puss', ['All'], 'female');
+const men = new Men ('Tom', ['Jack', 'Puss'], 'male', 'Hello');
+const woman = new SuperWoman ('Leya', ['Jack', 'Puss', 'All'], 'female', 'Karamba')
+
+const inhabitants = [dog, cat];
+inhabitants.forEach(item => print(item.getDescription()));
+
+const humans = [men, woman];
+humans.forEach(item => print(item.getDescriptionPeople()));
 
 
 // ======== OUTPUT ========
